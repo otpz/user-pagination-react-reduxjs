b2c23cd0f5723dc6e8143d01935621d8c369104b
Osman Topuz <osmantopuz98@gmail.com>
2024-09-04 17:17:03 +0300

    user active styling

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,38 +1,41 @@
+import { useState } from 'react';
 import { UserType } from '../../types/UserType';
 import styles from './styles.module.css'
 import { BiMailSend, BiPhone } from "react-icons/bi";
 
-
 interface Props {
     userData: UserType,
+    activeUserId: number
 }
 
-const User: React.FC<Props> = ({userData}) => {
-  return (
-    <div className={styles.container}>
-        <div className={styles.card}>
-            <div className={styles.img_div}>
-                <img className={styles.img} src={userData.image} alt={`${userData.firstName} ${userData.lastName}`} />
-            </div>
-            <div className={styles.info}>
-                <span>{userData.firstName} {userData.lastName}</span>
-                <span className={styles.job}>{userData.company.title}</span>
-            </div>
-            <hr className={styles.hr}/>
-            <div className={styles.contact}>
-                <a className={styles.email} href={`mailto: ${userData.email}`}>
-                    <span>E-mail</span>
-                    <BiMailSend className={styles.mail_icon}/>
-                </a>
-                
-                <a href={`tel:${userData.phone}`}>
-                    <span>{userData.phone}</span>
-                    <BiPhone className={styles.phone_icon}/>
-                </a>
+const User: React.FC<Props> = ({userData, activeUserId}) => {
+
+
+    const activeCard = activeUserId === userData.id ? `${styles.card} ${styles.active}` : `${styles.card}` 
+    return (
+        <div className={styles.container}>
+            <div className={activeCard}>
+                <div className={styles.img_div}>
+                    <img className={styles.img} src={userData.image} alt={`${userData.firstName} ${userData.lastName}`} />
+                </div>
+                <div className={styles.info}>
+                    <span>{userData.firstName} {userData.lastName}</span>
+                    <span className={styles.job}>{userData.company.title}</span>
+                </div>
+                <hr className={styles.hr}/>
+                <div className={styles.contact}>
+                    <a className={styles.email} href={`mailto: ${userData.email}`}>
+                        <span>E-mail</span>
+                        <BiMailSend className={styles.mail_icon}/>
+                    </a>
+                    <a href={`tel:${userData.phone}`}>
+                        <span>{userData.phone}</span>
+                        <BiPhone className={styles.phone_icon}/>
+                    </a>
+                </div>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default User
--- src/components/UserList/UserList.tsx
+++ src/components/UserList/UserList.tsx
@@ -1,10 +1,18 @@
 import styles from './styles.module.css'
 import User from '../User/User'
 import useFetch from '../../hooks/useFetch'
+import { useState } from 'react'
 
 const UserList = () => {
 
   const {data, loading, error} = useFetch('https://dummyjson.com/users?limit=6&skip=0')
+
+  const [activeUserId, setActiveUserId] = useState<number>(1)
+
+  const handleUserDetail = (id: number) => {
+    console.log("user id:", id)
+    setActiveUserId(id)
+  }
 
   if (loading){
     return (
@@ -21,17 +29,14 @@ const UserList = () => {
       </div>
     )
   }
-
-  if(!data){
-    return <div>Pending..</div>
-  }
-
   return (
     <div className={styles.container}>
       {
         data.map((userData, idx) => {
           return (
-            <User key={idx} userData={userData}/>
+            <div key={idx} onClick={() => handleUserDetail(userData.id)}>
+              <User userData={userData} activeUserId={activeUserId}/>
+            </div>
           )
         })
       }
